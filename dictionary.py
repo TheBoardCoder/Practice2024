4a809e2069e4d315da339131ed22e8ecc8bba39c
TheBoardCoder <89057781+TheBoardCoder@users.noreply.github.com>
2024-08-31 18:56:31 -0400

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,27 @@
+# using a dictionary  joel moore 4 october 2017
+
+'''
+program illustrates the use of a dictionary in counting the occurances of
+the letter in a phrase
+
+'''
+def main2():
+  print('hello world')
+  
+
+def hist(phrase):
+  d = dict()
+  for i in phrase:
+    d[i] = d.get(i, 0)+1
+  return d  
+
+def main():
+  main2()
+  phrase = input('enter a phrase ')
+  
+  count = hist(phrase)
+  print(count)
+
+main() 
+
+
